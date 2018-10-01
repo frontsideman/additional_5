21cbcbfc2da349e53400b22c76f55437ef7419be
Aliaksandr <alexex.by@gmail.com>
2018-10-02 00:03:48 +0300

    Correct expression Implement

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let newStr = str;
+  let isTrue = false;
+  let unicElemArr = [];
+  let bracketsArr = [];
+  const specialSymbols = '[ \ ^ $ . | ? * + ( )'.split(' ');
+  const checkIsSpecialSymbol = str => specialSymbols.includes(str);
+
+  if (str.length%2 !== 0) {
+    return false
+  }
+
+  findBrackets = (stroke, brackets) => {
+    if (stroke.length === 0) {
+      isTrue = true;
+    }
+    if (stroke.indexOf(brackets) !== -1 ) {
+      newStr = stroke.replace(brackets, '');
+      findBrackets(newStr, brackets)
+    } else {
+      return stroke;
+    }
+  }
+
+  Array.from(bracketsConfig).forEach(item => {
+    let firstBracket = item[0];
+    let lastBracket = item[1];
+    let pairBracket = firstBracket + lastBracket;
+    bracketsArr.push(pairBracket);
+  });
+
+  for (let i = 0, len = bracketsArr.length + 1; i < len; i++) {
+    bracketsArr.forEach(item => {
+      findBrackets(newStr, item);
+    });
+  }
+
+  return isTrue;
 }
